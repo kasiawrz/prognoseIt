8a642f0725bf2509e9a1fcdac0938376001980cc
Anna <ankaburkat@gmail.com>
2017-09-05 19:42:06 +0200

    section about and success

---
+++ src/scripts/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  var window_width = $( window ).width();
+
+  if(window_width <= 600) {
+    var box_width = $('.rules__box').width();
+    $('.rules__box').height(box_width);
+
+    var grid_width = $('.rules__grid').width();
+    var margin = (grid_width * 0.1);
+    $('.rules__box--top').css("margin-bottom", margin);
+
+    $('.rules__text').css("display", "none");
+  } else if (window_width > 600)  {
+    $('.rules__text').css("display", "inline");
+  }
+
+
+      $('.carousel').carousel({
+            dist:0,
+            shift:0,
+            padding:20,
+indicators: true
+      });
+
+
+
+  $(window).resize(function() {
+    var window_width = $( window ).width();
+    if(window_width <= 600) {
+
+      var box_width = $('.rules__box').width();
+      $('.rules__box').height(box_width)
+
+      var grid_width = $('.rules__grid').width();
+      var margin = (grid_width * 0.1);
+      $('.rules__box--top').css("margin-bottom", margin);
+
+      $('.rules__text').css("display", "none");
+    }else{
+        $('.rules__text').css("display", "inline");
+    }
+  });
+});
